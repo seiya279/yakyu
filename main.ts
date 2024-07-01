0f9a8e75bb335d3a5bcdca66876e4cb47b74b530
seiya279 <seiya.h313@gmail.com>
2024-07-01 11:21:29 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let pos_x = 2
+let pos_y = 4
+led.plot(pos_x, pos_y)
+basic.forever(function () {
+	
+})
