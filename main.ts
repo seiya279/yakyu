4351626300ce2ad88f91e13072e4ec4d67f26d22
seiya279 <seiya.h313@gmail.com>
2024-07-01 11:30:47 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-    	
+    if (0 < pos_x) {
+        led.unplot(pos_x, pos_y)
+        pos_x += -1
+        led.plot(pos_x, pos_y)
     }
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    for (let i = 0; i <= 3; i++) {
+        led.plot(pos_x, 3 - i)
+        basic.pause(100)
+        led.unplot(pos_x, 3 - i)
+    }
+    radio.sendNumber(pos_x)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    if (pos_x < 4) {
+        led.unplot(pos_x, pos_y)
+        pos_x += 1
+        led.plot(pos_x, pos_y)
+    }
 })
-let pos_x = 2
-let pos_y = 4
+let pos_y = 0
+let pos_x = 0
+pos_x = 2
+pos_y = 4
 led.plot(pos_x, pos_y)
 basic.forever(function () {
 	
